82280e6c14bb5ccaa89f575e9b9339951c7c05e6
Michiel <michiel@valkdigital.nl>
2023-10-27 22:07:28 +0200

    Day 4 - part 2

---
+++ day_4/ranges_partially_overlap.exs
@@ -0,0 +1,52 @@
+defmodule RangesPartiallyOverlap do
+  @moduledoc false
+
+  def run do
+    input_to_data("./input.txt")
+    |> filter_overlapping_ranges()
+    |> length()
+    |> IO.inspect()
+
+    IO.puts(IO.ANSI.format([:red_background, "Done!"]))
+  end
+
+  defp input_to_data(file) do
+    File.stream!(file)
+    # Get each line
+    |> Enum.map(fn line ->
+      line
+      # Trim string
+      |> String.trim_trailing("\n")
+      # Split string at `,`
+      |> String.split(",")
+      # Get range of each member of pair
+      |> Enum.map(fn range ->
+        # Convert range to list of numbers (section to clean)
+        [range_start | range_end] =
+          range
+          |> String.split("-")
+
+        range_start = range_start |> to_string() |> String.to_integer()
+        range_end = range_end |> to_string() |> String.to_integer()
+
+        Range.new(range_start, range_end, 1)
+      end)
+      |> List.to_tuple()
+    end)
+  end
+
+  defp filter_overlapping_ranges(ranges) do
+    # Remove numbers present in both members of pair
+    # Count empty lists
+    Enum.map(ranges, fn {range1, range2} = range ->
+      if Range.disjoint?(range1, range2) do
+        []
+      else
+        range
+      end
+    end)
+    |> List.flatten()
+  end
+end
+
+RangesPartiallyOverlap.run()
